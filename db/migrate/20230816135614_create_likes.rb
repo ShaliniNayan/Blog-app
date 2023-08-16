d432d50f17da5f3556bc20b6155584675a452f94
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-16 22:02:27 +0530

    Fix foreign key error

--- db/migrate/20230816135614_create_likes.rb
+++ db/migrate/20230816135614_create_likes.rb
@@ -1,8 +1,8 @@
 class CreateLikes < ActiveRecord::Migration[7.0]
   def change
     create_table :likes do |t|
-      t.integer :author_id
-      t.integer :post_id
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
 
       t.timestamps
     end
--- db/migrate/20230816135854_create_comments.rb
+++ db/migrate/20230816135854_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.integer :author_id
-      t.integer :post_id
+      t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: { to_table: :posts }
       t.text :text
 
       t.timestamps
