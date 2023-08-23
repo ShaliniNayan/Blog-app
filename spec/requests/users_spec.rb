a944d9c88d670939e3041ca608024e1e4f0f87e0
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-23 21:21:31 +0530

    Fix linters

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe UsersController, type: :controller do
 
   describe 'GET #show' do
     it 'returns http success' do
-      get user_path (1)
+      get user_path(1)
       expect(response).to have_http_status(:success)
     end
   end
