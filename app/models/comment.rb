92bdc3e6c0d02923a07a2b82ae1992e9e89296f7
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-24 17:36:30 +0530

    Update comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,10 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  belongs_to :post, class_name: 'Post'
+  belongs_to :post
 
-  attribute :text, :text
+  before_save :update_comments_counter
 
-  after_create :update_post_comments_counter
-  after_destroy :update_post_comments_counter
-
-  def update_post_comments_counter
-    post.update(comments_counter: post.comments.count)
+  def update_comments_counter
+    post.increment!(:comments_counter)
   end
 end
