31cd7961e00ce6b3c11f9d06225c613d576d9f36
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-24 17:35:11 +0530

    Update post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,17 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    page = params[:page] || 1
+    per_page = 10
+
+    @posts = Post.includes(:author)
+      .includes(:comments)
+      .where(author: params[:user_id])
+      .order(created_at: :asc)
+      .offset((page.to_i - 1) * per_page)
+      .limit(per_page)
+
+    @total_pages = (Post.count.to_f / per_page).ceil
+    @author = @posts.first.author
   end
 
   def show
