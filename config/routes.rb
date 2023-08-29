7b2cc2f3b8d42d053d23862e885aaec650fba2f6
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-29 22:35:00 +0530

    Add feature folder in spec and run the test for vieew user

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
   root "users#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :likes, only: [:create]
-      resources :comments, only: [:create] # Add this line to create comments
+      resources :comments, only: [:new, :create] 
     end
   end
 end
