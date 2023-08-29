f5069d92fb039bd9c887c0e568477679a5168841
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-29 22:53:05 +0530

    Fix linters

--- spec/features/user_show_spec.rb
+++ spec/features/user_show_spec.rb
@@ -11,15 +11,11 @@ RSpec.feature 'User Show', type: :feature do
     expect(page).to have_content('Tom')
     expect(page).to have_content('Bio')
     expect(page).to have_content('He is a good programmer')
-
-
     expect(page).to have_link('See all posts')
-
     expect(page).to have_content('first post')
     expect(page).to have_link('first post', href: user_post_path(user, post1))
     expect(page).to have_content('second post')
     expect(page).to have_link('second post', href: user_post_path(user, post2))
-
     expect(page).to have_content('Recent Posts') if page.has_content?('Recent Posts')
   end
 end
