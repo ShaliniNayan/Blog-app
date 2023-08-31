418516bf2c332ae83b6253e3f3238da42c1a9784
Shalini Nayan <117961613+ShaliniNayan@users.noreply.github.com>
2023-08-31 15:47:12 +0530

    Update Application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,15 @@
 class ApplicationController < ActionController::Base
-  before_action :set_current_user
+ 
+    before_action :configure_permitted_parameters, if: :devise_controller?
 
-  private
-
-  def set_current_user
-    user_id = params[:user_id]
-    @current_user = User.find_by(id: user_id)
-  end
-
-  def require_user
-    redirect_to root_path unless @current_user
-  end
-
-  attr_reader :current_user
-
-  helper_method :current_user
+    def after_sign_in_path_for(_resource)
+      users_path
+    end
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :bio, :password, :password_confirmation])
+    end
+  
 end
